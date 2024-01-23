86783f349842f488ffb94cb4d295560ebe152231
luubang201 <dangvuluu201@gmail.com>
2024-01-23 16:51:48 +0700

    update

--- demo/controller/ListStudentController.js
+++ demo/controller/ListStudentController.js
@@ -1,12 +1,16 @@
 window.ListStudentController = function ($scope, $http){
     $scope.title = "Danh sách sinh viên";
 
-    // Call api
+    // Thực hiện việc call API
     const apistudents = "http://localhost:3000/student";
+
+    // Tham số $http (là giao thức để thực hiện việc call api)
+    // $http.phương thức sử dụng(link api).then()
     function getData() {
         $http.get(apistudents).then(function(response){
             console.log(response);
 
+            // Truy cập vào dữ liệu
             console.log(response.data);
             if (response.status == 200 ){
                 $scope.liststudents = response.data;
